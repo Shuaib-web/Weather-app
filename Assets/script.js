f0e44438048e4c7746c6edd86eeea3015316cef8
shuib <shuaib_97@hotmail.co.uk>
2023-01-02 16:03:24 +0000

    Updating html

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 let weather = {
     "apiKey": "4e323e25a41eb88616b82e8715147146",
     fetchWeather: function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&limit=5&appid=4e323e25a41eb88616b82e8715147146")
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&limit&units=metrics=5&appid=4e323e25a41eb88616b82e8715147146")
         .then((response)=> response.json())
         .then((data) => this.displayWeather(data));
     } ,
@@ -10,12 +10,36 @@ let weather = {
         const{ icon, description} = data.weather[0];
         const{ temp, humidity } = data.main;
         const{speed} = data.wind;   
-        console.log(name, icon,description, temp, humidity)
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
-        
-    }}
+        document.querySelector(".wind").innerText ="Wind speed: " + speed + " mp/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+  document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Denver");
